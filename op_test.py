784c570925a334f3dbcda9a5614efd1c5b05f74f
Nathanael Merlin <nate@merlin.uno>
2018-10-19 22:57:56 -0700

    Fix most pylint errors in op_test.py, leaving op C0103 error

--- op_test.py
+++ op_test.py
@@ -1,34 +1,41 @@
 #!/usr/bin/python
+""" Unit tests for op.py, assumes previous run of op """
 
 import unittest
 import os
 import json
-from op import OnePassword
 from pathlib import Path
+from op import OnePassword
 
 
-class testInit(unittest.TestCase):
+class TestInit(unittest.TestCase):
+    """ Tests OnePassword methods """
     def test_constructor_config_empty(self):
+        """ Tests the constructor method with an empty string """
         op = OnePassword()
         self.assertEqual(op.config_path, str(Path.home()) + '/.op/config')
 
-    def test_constructor_config_string(self):
+    def test_constructor_op_string(self):
+        """ Tests the constructor method with the path to .op """
         op = OnePassword('/home/nmerlin/.op')
         self.assertEqual(op.config_path, str(Path.home()) + '/.op/config')
 
     def test_constructor_config_read(self):
+        """ Tests reading the configuration file """
         op = OnePassword()
-        with open(str(Path.home()) + '/.op/config') as f:
-            contents = f.read()
+        with open(str(Path.home()) + '/.op/config') as config_file:
+            contents = config_file.read()
             self.assertEqual(op.config, json.loads(contents))
 
     def test_signin(self):
+        """ Tests signing in/reading from the sign-in cache """
         op = OnePassword()
         op.sign_in()
         self.assertEqual(len(op.session_tokens), len(op.config['accounts']))
         self.assertTrue(os.path.isfile(op.session_cache))
 
     def test_list_items(self):
+        """ Tests the wrapper around op list items """
         op = OnePassword()
         op.sign_in()
         items = op.list_items()
