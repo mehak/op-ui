71fc3c234d350cbdfa3cc966cc7131030bdc89c8
Nathanael Merlin <nate@merlin.uno>
2018-10-20 16:06:43 -0700

    Add times for tests

--- op_test.py
+++ op_test.py
@@ -4,12 +4,21 @@
 import unittest
 import os
 import json
+import time
 from pathlib import Path
 from op import OnePassword
 
-
 class TestInit(unittest.TestCase):
     """ Tests OnePassword methods """
+    def setUp(self):
+        """ Setup tests """
+        self.startTime = time.time()
+
+    def tearDown(self):
+        """ Tear down tests and print run time """
+        duration = time.time() - self.startTime
+        print(f'{self.id()}: {duration}')
+
     def test_constructor_config_empty(self):
         """ Tests the constructor method with an empty string """
         op = OnePassword()
@@ -41,5 +50,13 @@ class TestInit(unittest.TestCase):
         items = op.list_items()
         self.assertTrue(len(items) > 0)
 
+    def test_list_documents(self):
+        """ Tests the wrapper around op list documents """
+        op = OnePassword()
+        op.signin()
+        documents = op.list_documents()
+        self.assertTrue(len(documents) > 0)
+
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestInit)
+    unittest.TextTestRunner(verbosity=0).run(suite)
