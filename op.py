674ccfc8fb7ccfe1394649197964c9cfa57b5735
Nathanael Merlin <nate@merlin.uno>
2018-10-20 16:05:01 -0700

    Move cache, fix sesison bug

--- op.py
+++ op.py
@@ -11,37 +11,41 @@ class OnePassword:
     """ Class responsible for wrapping op """
     def __init__(self, op_path=None):
         self.command = '/usr/bin/op'
+        self.namespace = 'win.b1n.op'
 
         if op_path:
             self.op_path = op_path
         else:
             home = str(Path.home())
-            self.op_path = home + '/.op'
+            self.op_path = f'{home}/.op'
 
-        self.config_path = self.op_path + '/config'
-        self.session_cache = self.op_path + '/session_cache'
+        self.config_path = f'{self.op_path}/config'
+        self.session_cache = f'/tmp/{self.namespace}_session_cache'
 
         with open(self.config_path) as config_file:
             self.config = json.loads(config_file.read())
 
         self.session_tokens = []
 
-    def signin(self, subdomain='all', use_cache=True):
-        """ Either read from a cache or sign-in to all accounts """
-
+    def aged_out(self):
+        """ Checks the age of the cache """
         now = datetime.now().timestamp()
-        last_access = os.stat(self.config_path).st_atime
+        last_access = os.stat(self.session_cache).st_atime
         seconds_since_last_access = now - last_access
         thirty_minutes = 1800
-        old_cache = seconds_since_last_access >= thirty_minutes
 
-        if use_cache and os.path.isfile(self.session_cache) and not old_cache:
+        return seconds_since_last_access >= thirty_minutes
+
+    def signin(self, subdomain='all', use_cache=True):
+        """ Either read from a cache or sign-in to all accounts """
+        if use_cache and os.path.isfile(self.session_cache) and not self.aged_out():
             with open(self.session_cache) as session_file:
                 self.session_tokens = json.loads(session_file.read())
 
                 for token in self.session_tokens:
                     os.environ['OP_SESSION_' + token['subdomain']] = token['token']
         else:
+            self.session_tokens = []
             for account in self.config['accounts']:
                 subdomain = account['shorthand']
                 command = [self.command, 'signin', subdomain, '--output=raw']
@@ -58,6 +62,10 @@ class OnePassword:
         """ Helper for running op commands and returning output objects """
         command = [self.command] + arguments
         output = run(command, capture_output=True)
+
+        if output.returncode > 0:
+            self.signin(use_cache=False)
+            self.__generic_run(arguments)
 
         return json.loads(output.stdout.decode('utf8').rstrip('\n'))
 
@@ -78,3 +86,7 @@ class OnePassword:
     def list_items(self):
         """ Get a list of items from signed in accounts """
         return self.__generic_list_all('items')
+
+    def list_documents(self):
+        """ Get a list of documents from signed in accounts """
+        return self.__generic_list_all('documents')
